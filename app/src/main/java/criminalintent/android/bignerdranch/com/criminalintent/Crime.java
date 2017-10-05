c84c8c8dbfd88a8e0e363f712e8548c86423e1a3
Kimberly Shen <kshen@pinterest.com>
2017-10-05 10:34:12 -0700

    adding more details for crimes

--- app/src/main/java/criminalintent/android/bignerdranch/com/criminalintent/Crime.java
+++ app/src/main/java/criminalintent/android/bignerdranch/com/criminalintent/Crime.java
@@ -1,5 +1,6 @@
 package criminalintent.android.bignerdranch.com.criminalintent;
 
+import java.util.Date;
 import java.util.UUID;
 
 /**
@@ -11,10 +12,13 @@ public class Crime {
 
     private UUID mId;
     private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
 
     public Crime() {
         // Generate unique identifier
         mId = UUID.randomUUID();
+        mDate = new Date();
     }
 
     public String getTitle() {
@@ -27,5 +31,21 @@ public class Crime {
 
     public UUID getId() {
         return mId;
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date mDate) {
+        this.mDate = mDate;
+    }
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean mSolved) {
+        this.mSolved = mSolved;
     }
 }
